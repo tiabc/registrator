ba841815f8e074d6b97397f1ae73bd1d92a2cc71
Ivan Tomilov <tiabc.work@gmail.com>
2016-11-09 14:51:58 +0300

    Implemented a negative test case

--- test/registrator.js
+++ test/registrator.js
@@ -1,21 +1,37 @@
 contract('Registrator', function(accounts) {
-  it("should register a user and return his record", function() {
+  var zeroAddress = '0x0000000000000000000000000000000000000000';
+
+  var ownerAccount = accounts[0];
+  it("should successfully register a user", function() {
     var reg = Registrator.deployed();
 
-    return reg.GetRecord.call(0x123456).then(function(res) {
+    return reg.GetRecord.call(0x123456, {from: ownerAccount}).then(function(res) {
       var sender = res[0];
-      var time = res[1];
-      assert.equal(sender, '0x0000000000000000000000000000000000000000', "sender must be initially empty");
+      var time   = res[1];
+      assert.equal(sender, zeroAddress, "sender must be initially empty");
       assert.equal(time.toNumber(), 0, "no record must be initially stored");
     }).then(function() {
-      reg.register(0x123456);
-      return reg.GetRecord.call(0x123456);
+      reg.register(0x123456, {from: ownerAccount});
+      return reg.GetRecord.call(0x123456, {from: ownerAccount});
     }).then(function(res) {
       var sender = res[0];
-      var time = res[1];
+      var time   = res[1];
+      assert.equal(sender, ownerAccount);
       assert.notEqual(time.toNumber(), 0, "time mustn't be 0 for a registered record");
     });
   });
 
-  // TODO: Test if the person not owning the contract is able to store data there.
+  var sideAccount  = accounts[1];
+  it("should be unable to register a user", function() {
+    var reg = Registrator.deployed();
+
+    return reg.register(0x654321, {from: sideAccount}).then(function() {
+      return reg.GetRecord.call(0x654321, {from: sideAccount});
+    }).then(function(res) {
+      var sender = res[0];
+      var time   = res[1];
+      assert.equal(sender, zeroAddress);
+      assert.equal(time.toNumber(), 0, "time must be 0 for an unregistered record");
+    });
+  });
 });
